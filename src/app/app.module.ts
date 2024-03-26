9393c8e110e0d57b2ab2270a3a44c4dbcfa733ad
mohamedderouiche <mohamedamine.derouiche@esprit.tn>
2024-03-26 22:52:47 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoyerComponent } from './foyer/foyer/foyer.component';
+import { Foyer } from './models/Foyer';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ShowfoyerComponent } from './foyer/showfoyer/showfoyer.component';
+import { UpdatefoyerComponent } from './foyer/updatefoyer/updatefoyer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoyerComponent,
+    ShowfoyerComponent,
+    UpdatefoyerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [Foyer],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
